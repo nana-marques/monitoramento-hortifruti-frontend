97de91730305cc0437a6b230c7c58edaef70d34c
prazeresmath <matheus.prazeres@hotmail.com>
2024-10-01 16:21:09 -0300

    feat: side menu e animação

--- src/assets/navbar/index.jsx
+++ src/assets/navbar/index.jsx
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { motion } from "framer-motion";
 import './styles.css';
 
-const Navbar = () =>{
-    return(
-    <div className="container-navbar">
-        <ul className="navbar">
-            <li className="item">
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <div className="container-navbar">
+            <button className="menu-button" onClick={toggleMenu}>
                 <div className="hamburguer"></div>
                 <div className="hamburguer"></div>
                 <div className="hamburguer"></div>
-            </li>
-            <li className="item"><p>Monitoramento de Gondolas</p></li>
-        </ul>
-    </div>
-  )
-}
-export default Navbar
+            </button>
+            <motion.div 
+                className="sidebar"
+                initial={{ x: '-100%' }}
+                animate={{ x: isOpen ? 0 : '-100%' }}
+                transition={{ type: 'spring', stiffness: 100 }}
+            >
+                <button className="close-button" onClick={toggleMenu}>X</button>
+                <ul className="navbar">
+                    <li className="item">
+                        <p>Monitoramento de Gondolas</p>
+                    </li>
+                    <li className="item">
+                        <a href="#">Opção 1</a>
+                    </li>
+                    <li className="item">
+                    <a href="#">Opção 2</a>
+                    </li>
+                    <li className="item">
+                    <a href="#">Opção 3</a>
+                    </li>
+                </ul>
+            </motion.div>
+        </div>
+    );
+};
+
+export default Navbar;
